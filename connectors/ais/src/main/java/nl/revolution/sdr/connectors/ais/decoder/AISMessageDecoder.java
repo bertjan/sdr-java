a967837be52edc8db840960dd6fcdf0c62975917
Bert Jan Schrijver <bertjan@revolution.nl>
2015-04-25 15:22:22 +0200

    Restructure AIS decoder to align with ADS-B decoder.

---
+++ connectors/ais/src/main/java/nl/revolution/sdr/connectors/ais/decoder/AISMessageDecoder.java
@@ -0,0 +1,42 @@
+package nl.revolution.sdr.connectors.ais.decoder;
+
+import dk.tbsalling.aismessages.ais.messages.PositionReport;
+import dk.tbsalling.aismessages.ais.messages.types.AISMessageType;
+import dk.tbsalling.aismessages.nmea.NMEAMessageHandler;
+import dk.tbsalling.aismessages.nmea.exceptions.NMEAParseException;
+import dk.tbsalling.aismessages.nmea.messages.NMEAMessage;
+import nl.revolution.sdr.services.positiondata.api.PositionData;
+import nl.revolution.sdr.services.positiondata.api.PositionDataService;
+
+public class AISMessageDecoder {
+
+    private PositionDataService positionDataService;
+
+    public AISMessageDecoder(PositionDataService positionDataService) {
+        this.positionDataService = positionDataService;
+    }
+
+    public void processAISMessage(String aisMessageString) {
+        try {
+            new NMEAMessageHandler("RTL-SDR", aisMessage -> {
+                AISMessageType messageType = aisMessage.getMessageType();
+                if (messageType == AISMessageType.PositionReportClassAScheduled ||
+                        messageType == AISMessageType.PositionReportClassAAssignedSchedule ||
+                        messageType == AISMessageType.PositionReportClassAResponseToInterrogation) {
+                    PositionReport positionMessage = (PositionReport) aisMessage;
+
+                    positionDataService.positionDataReceived(new PositionData()
+                            .withType(PositionData.ObjectType.SHIP)
+                            .withObjectId(String.valueOf(positionMessage.getSourceMmsi().getMMSI()))
+                            .withLongitude(positionMessage.getLongitude())
+                            .withLatitude(positionMessage.getLatitude())
+                            .withHeading(positionMessage.getCourseOverGround())
+                            .withTimestamp(System.currentTimeMillis()));
+                }
+            }).accept(NMEAMessage.fromString(aisMessageString));
+        } catch (NMEAParseException e) {
+            // Do nothing.
+        }
+    }
+
+}
--- connectors/ais/src/main/java/nl/revolution/sdr/connectors/ais/server/Main.java
+++ connectors/ais/src/main/java/nl/revolution/sdr/connectors/ais/server/Main.java
@@ -1,5 +1,8 @@
 package nl.revolution.sdr.connectors.ais.server;
 
+import nl.revolution.sdr.connectors.ais.decoder.AISDecoderSource;
+import nl.revolution.sdr.services.positiondata.api.PositionDataService;
+import nl.revolution.sdr.services.positiondata.mongodb.MongoPositionDataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,10 +11,14 @@ public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
+        PositionDataService dataService = new MongoPositionDataService();
+        AISDecoderSource dataServer = new AISDecoderSource(dataService);
+        dataServer.start();
 
         LOG.info("AIS connector started. Press enter to quit.");
         System.in.read();
 
+        dataServer.stop();
     }
 
 }
